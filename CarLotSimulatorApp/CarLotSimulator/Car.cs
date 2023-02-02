9575343b327803b8bf6722c2d341bc624464eccd
Diamond Mccall <singlikediamond@gmail.com>
2023-02-01 21:42:22 -0600

    inital commit

---
+++ CarLotSimulatorApp/CarLotSimulator/Car.cs
@@ -0,0 +1,39 @@
+﻿using System;
+namespace CarLotSimulator
+{
+	public class Car
+	{
+
+		public Car()
+		{
+
+		}
+		public Car(int year, string make, string model, string engineNoise, string honkNoise, string isDriveable)
+
+		{
+			Year = year;
+			Make = make;
+			Model = model;
+			EngineNoise = engineNoise;
+			HonkNoise = honkNoise;
+			IsDriveable = isDriveable;
+		}
+		
+		public string Make { get; set; }
+		public int Year { get; set; }
+		public string Model { get; set; }
+		public string EngineNoise { get; set; }
+		public string HonkNoise { get; set; }
+		public string IsDriveable { get; set; }
+
+		public void MakeEngineNoise(string EngineNoise)
+		{
+			Console.WriteLine($"{EngineNoise}");
+		}
+		public void MakeHonkingNoise(string HonkingNoise)
+		{
+			Console.WriteLine($"{HonkNoise}");
+		}
+    }
+}
+
--- CarLotSimulatorApp/CarLotSimulator/Program.cs
+++ CarLotSimulatorApp/CarLotSimulator/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace CarLotSimulator
 {
@@ -17,6 +18,52 @@ namespace CarLotSimulator
             //Now that the Car class is created we can instanciate 3 new cars
             //Set the properties for each of the cars
             //Call each of the methods for each car
+
+            var Lot = new CarLot();
+
+            var Ford = new Car();
+            Ford.Year = 2009;
+            Ford.Make = "Ford";
+            Ford.Model = "Focus";
+            Ford.HonkNoise = "Beep";
+            Ford.EngineNoise = "Vroom";
+            Ford.IsDriveable = "Yes";
+
+            Lot.Cars.Add(Ford);
+
+            var Infiniti = new Car()
+            {
+                Year = 2011,
+                Make = "Infiniti",
+                Model = "GS37",
+                HonkNoise = "Loud",
+                EngineNoise = "Quiet",
+                IsDriveable = "Yes",
+
+            };
+            Lot.Cars.Add(Infiniti);
+
+            var DreamCar = new Car(2022, "Chevrolet", "Corvette","Quiet","None","Yes");
+
+            Lot.Cars.Add(DreamCar);
+
+            foreach (var car in Lot.Cars)
+            {
+                Console.WriteLine($" Year {car.Year}, Make {car.Make}, Model {car.Model}");
+
+            }
+
+
+
+
+
+
+
+
+
+
+
+
 
             //*************BONUS*************//
 
